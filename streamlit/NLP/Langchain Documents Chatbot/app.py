0495142009fa142054494216e659d9f05f3ce962
unknown <2569618533@qq.com>
2023-10-03 14:49:15 -0700

    documents chat

---
+++ streamlit/NLP/Langchain Documents Chatbot/app.py
@@ -0,0 +1,162 @@
+import streamlit as st
+import os
+import tempfile
+
+from streamlit_chat import message
+from langchain.chains import ConversationalRetrievalChain
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.llms import CTransformers, Replicate
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.document_loaders import PyPDFLoader, TextLoader, Docx2txtLoader
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def initialize_session_state():
+    if "history" not in st.session_state:
+        st.session_state["history"] = []
+
+    if "generated" not in st.session_state:
+        st.session_state["generated"] = ["Hello! Ask me anything about 🤗"]
+
+    if "past" not in st.session_state:
+        st.session_state["past"] = ["Hey! 👋"]
+
+
+def conversation_chat(query, chain, history):
+    result = chain({
+        "question": query,
+        "chat_history": history
+    })
+    history.append((query, result["answer"]))
+    return result["answer"]
+
+
+def display_chat_history(chain):
+    reply_container = st.container()
+    container = st.container()
+
+    with container:
+        with st.form(key="my_form", clear_on_submit=True):
+            user_input = st.text_input(
+                "Question:",
+                placeholder="Ask about your Documents",
+                key="input"
+            )
+            submit_button = st.form_submit_button(label="Send")
+
+        if submit_button and user_input:
+            with st.spinner("Generating response ......"):
+                output = conversation_chat(
+                    query=user_input,
+                    chain=chain,
+                    history=st.session_state["history"]
+                )
+
+            st.session_state["past"].append(user_input)
+            st.session_state["generated"].append(output)
+
+    if st.session_state["generated"]:
+        with reply_container:
+            for i in range(len(st.session_state["generated"])):
+                message(
+                    st.session_state["past"][i],
+                    is_user=True,
+                    key=str(i) + "_user",
+                    avatar_style="thumbs"
+                )
+                message(
+                    st.session_state["generated"][i],
+                    key=str(i),
+                    avatar_style="fun-emoji"
+                )
+
+
+def create_conversational_chain(vector_store):
+    load_dotenv()
+
+    llm = Replicate(
+        model="",
+        streaming=True,
+        callbacks=[StreamingStdOutCallbackHandler()],
+        input={
+            "temperature": 0.1,
+            "max_length": 512,
+            "top_p": 1
+        }
+    )
+
+    memory = ConversationBufferMemory(
+        memory_key="chat_history",
+        return_messages=True
+    )
+
+    chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        chain_type="stuff",
+        retriever=vector_store.as_retriever(search_kwargs={"k": 3}),
+        memory=memory
+    )
+
+    return chain
+
+
+def main():
+    load_dotenv()
+    initialize_session_state()
+    st.title("Multi-Docs ChatBot Using Llama2 :books:")
+    st.sidebar.title("Document Processing")
+    uploaded_files = st.sidebar.file_uploader(
+        "Upload files",
+        accept_multiple_files=True
+    )
+
+    if uploaded_files:
+        text = []
+        for file in uploaded_files:
+            file_extension = os.path.splitext(file.name)[1]
+            with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+                temp_file.write(file.read())
+                temp_file_path = temp_file.name
+
+            loader = None
+            if file_extension == ".pdf":
+                loader = PyPDFLoader(temp_file_path)
+            elif file_extension == ".docx" or file_extension == ".doc":
+                loader = Docx2txtLoader(temp_file_path)
+            elif file_extension == ".txt":
+                loader = TextLoader(temp_file_path)
+
+            if loader:
+                text.extend(loader.load())
+                os.remove(temp_file_path)
+
+        text_splitter = CharacterTextSplitter(
+            separator="\n",
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+        )
+        text_chunks = text_splitter.split_documents(text)
+
+        embedding = HuggingFaceEmbeddings(
+            model_name="sentence-transformers/all-MiniLM-L6-v2",
+            model_kwargs={"device": "cpu"}
+        )
+
+        vector_store = FAISS.from_documents(
+            documents=text_chunks,
+            embedding=embedding
+        )
+
+        chain = create_conversational_chain(vector_store=vector_store)
+
+        display_chat_history(chain=chain)
+
+
+if __name__ == "__main__":
+    main()
